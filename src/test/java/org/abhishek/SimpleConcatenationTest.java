10de9a390d6db018e03b24f06d4990cb6eb5b235
abhishekomp <abhishek2283@hotmail.com>
2025-08-31 22:28:10 +0200

    added assert statement in the test method

--- src/test/java/org/abhishek/SimpleConcatenationTest.java
+++ src/test/java/org/abhishek/SimpleConcatenationTest.java
@@ -4,6 +4,9 @@ import org.junit.Test;
 
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.assertEquals;
+
 /**
  * Created by sca820 on 16 mars, 2023
  */
@@ -13,12 +16,18 @@ public class SimpleConcatenationTest {
     public void performConcatenation() {
         SimpleConcatenation simpleConcatenation = new SimpleConcatenation();
         final String result = simpleConcatenation.performConcatenation("Hello", "World");
-        System.out.println("result = " + result);
+        //System.out.println("result = " + result);
+        // JUnit4 assertion
+        assertEquals("HelloWorld", result);
+        // AssertJ assertion
+        assertThat(result).isEqualTo("HelloWorld");
     }
 
     @Test
     public void print_list() {
         List<String> strings = List.of("I", "got", "the", "power");
         System.out.println(String.join(", ",strings));
+        // AssertJ assertion
+        assertThat(strings).isEqualTo(List.of("I", "got", "the", "power"));
     }
 }
